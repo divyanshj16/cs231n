6bb00305905225012a37a2e693c5d46ee3e647fb
Divyansh Jha <divyanshj.16@gmail.com>
2017-09-14 16:58:20 +0530

    complete softmax  classifier

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -30,7 +30,26 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+  for i in xrange(num_train):
+    pred = X[i].dot(W)
+    pred -= np.max(pred)
+    pred = np.exp(pred)/np.sum(np.exp(pred))
+    z = pred
+    loss += -np.log(pred[y[i]])
+    # print(z.shape)
+    one_hot_y_i = np.zeros(num_classes).reshape(-1,1)
+    one_hot_y_i[y[i]] = 1
+    z = z.reshape(-1,1)
+    temp = z - one_hot_y_i
+    # print(X[i].shape)
+    dW += X[i].reshape(-1,1).dot(temp.T)
+
+  loss /= num_train + 0.5 * reg * np.sum(W * W)
+  dW /= num_train + reg * W
+
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -54,10 +73,19 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_train = X.shape[0]
+  pred = X.dot(W)
+  pred = pred - np.max(pred,axis=1,keepdims=True)
+  pred = np.exp(pred)/np.sum(np.exp(pred),axis=1,keepdims=True)
+  loss = np.sum(-np.log(pred[range(num_train),list(y)]))/num_train + 0.5 *  reg * np.sum(W * W)
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
+  one_hot_y = np.zeros_like(pred)
+  one_hot_y[range(num_train),list(y)] = 1
+  temp = pred - one_hot_y
+  dW = X.T.dot(temp)/num_train
+
 
   return loss, dW
 
